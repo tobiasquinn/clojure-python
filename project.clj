ca334950c922ab1607d5ac360dccdbe5ae8b625a
Tobias Quinn <tobias@tobiasquinn.com>
2015-11-16 00:36:31 +0000

    use jython 2.7.0 and clojure 1.7.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clojure-python "0.4.1"
+(defproject clojure-python "0.4.2"
   :description "Improve seamlessness of Clojure Jython interop."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.python/jython-standalone "2.5.3"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.python/jython-standalone "2.7.0"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
